d84eeec2b4b02d0110f23729c8655dfd0af35228
18580764418 <43516768+18580764418@users.noreply.github.com>
2022-06-14 11:42:34 +0800

    vim /opt/spark/conf/spark-env.sh
    SPARK_LOCAL_IP是本机节点的hostname

---
+++ spark-env.sh
@@ -0,0 +1,11 @@
+export SCALA_HOME=/usr/local/scala
+export JAVA_HOME=/usr/local/jdk
+export HADOOP_HOME=/opt/hadoop
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export SPARK_HOME=/opt/spark
+export SPARK_MASTER_IP=master
+export SPARK_LOCAL_IP=master
+export SPARK_MASTER_HOST=master
+export SPARK_MASTER_PORT=7077
+export YARN_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export SPARK_YARN_USER_ENV=$HADOOP_HOME/etc/hadoop
