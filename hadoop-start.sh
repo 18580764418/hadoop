465ddb342c0f538d291b31eafb75260343144c17
18580764418 <43516768+18580764418@users.noreply.github.com>
2022-06-22 10:56:20 +0800

    Delete hadoop-start.sh

--- hadoop-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /opt/hadoop && sbin/start-dfs.sh && mapred --daemon start historyserver
-sleep 5 && hdfs dfsadmin -safemode leave &&sbin/start-yarn.sh
-cd /usr/local/spark/ && sbin/start-all.sh
-cd ~ 
-nohup hive --service metastore -p 9083 >> /dev/null 2>&1 &
-nohup  hive --service hiveserver2 >> /dev/null 2>&1 &
